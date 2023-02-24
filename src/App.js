fc9f50ba1f7472639f0a00a41837615d0687fc45
Mohammad Nafisi <nafisidev@gmail.com>
2023-02-24 13:57:11 -0800

    Adding the toggle button

--- src/App.js
+++ src/App.js
@@ -29,13 +29,22 @@ function App() {
 
       <div className="flex space-x-10 m-5">
         <div><h1 className='m-2 text-xl font-bold'>Name of the Habit: Reading Daily</h1></div>
-        <div>
-          <label class="relative inline-flex items-center cursor-pointer">
-            <input type="checkbox" value="" class="sr-only peer"/>
-            <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-            <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Count my Habit</span>
+       
+        <div class="flex items-center justify-center w-full mb-12">
+          <label for="toggleB" class="flex items-center cursor-pointer">
+            
+            <div class="relative">
+             
+              <input type="checkbox" id="toggleB" class="sr-only"/>
+              <div class="block bg-gray-600 w-14 h-8 rounded-full"></div>
+              <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
+            </div>
+            <div class="ml-3 text-gray-700 font-medium">
+              Habbit Type
+            </div>
           </label>
         </div>
+
         <div className='flex flex-col border-2 border-indigo-600'><p className='m-2 text-xl font-bold'>Monday</p><input type="checkbox" className='m-2'/></div>
         <div className='flex flex-col'><p className='m-2 text-xl font-bold'>Tuesday</p><input type="checkbox" className='m-2'/></div>
         <div className='flex flex-col'><p className='m-2 text-xl font-bold'>Wednesday</p><input type="checkbox" className='m-2'/></div>
