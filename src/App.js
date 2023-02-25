f7b89460168c78b0266fbfcd566e024903864e31
Mohammad Nafisi <nafisidev@gmail.com>
2023-02-25 15:01:02 -0800

    Two habit types

--- src/App.js
+++ src/App.js
@@ -7,10 +7,23 @@ function App() {
 
   const [count, setCount] = useState(0);
   const [habitcount, setHabitcount] = useState(0);
+  const [habittype, setHabittype] = useState(false);
+  const [moncount, setMoncount] = useState(0);
+  const [tuecount, setTuecount] = useState(0);
+  const [wedcount, setWedcount] = useState(0);
+  const [thucount, setThucount] = useState(0);
+  const [fricount, setFricount] = useState(0);
+  const [satcount, setSatcount] = useState(0);
+  const [suncount, setSuncount] = useState(0);
+
 
    useEffect(() => {
     document.title = `You clicked ${count} times`;
   });
+
+  function toggleButton(){
+    setHabittype(!habittype)
+  }
 
   return (
     <div>
@@ -35,7 +48,7 @@ function App() {
             
             <div class="relative">
              
-              <input type="checkbox" id="toggleB" class="sr-only"/>
+              <input type="checkbox" id="toggleB" onClick={toggleButton} class="sr-only"/>
               <div class="block bg-gray-600 w-14 h-8 rounded-full"></div>
               <div class="dot absolute left-1 top-1 bg-white w-6 h-6 rounded-full transition"></div>
             </div>
@@ -45,6 +58,8 @@ function App() {
           </label>
         </div>
 
+        {habittype ?
+        <div className='flex items-center'>
         <div className='flex flex-col border-2 border-indigo-600'><p className='m-2 text-xl font-bold'>Monday</p><input type="checkbox" className='m-2'/></div>
         <div className='flex flex-col'><p className='m-2 text-xl font-bold'>Tuesday</p><input type="checkbox" className='m-2'/></div>
         <div className='flex flex-col'><p className='m-2 text-xl font-bold'>Wednesday</p><input type="checkbox" className='m-2'/></div>
@@ -52,6 +67,18 @@ function App() {
         <div className='flex flex-col'><p className='m-2 text-xl font-bold'>Friday</p><input type="checkbox" className='m-2'/></div>
         <div className='flex flex-col'><p className='m-2 text-xl font-bold'>Saturday</p><input type="checkbox" className='m-2'/></div>
         <div className='flex flex-col'><p className='m-2 text-xl font-bold'>Sunday</p><input type="checkbox" className='m-2'/></div>
+        </div> :
+        <div className='flex items-center'>
+        <div className='flex flex-col border-2 border-indigo-600'><p className='m-2 text-xl font-bold'>Monday</p><p>{moncount}</p><button onClick={() => setMoncount(moncount + 1)}>Add to Monday</button></div>
+        <div className='flex flex-col'><p className='m-2 text-xl font-bold'>Tuesday</p><p>{tuecount}</p><button onClick={() => setTuecount(tuecount + 1)}>Add to Tuesday</button></div>
+        <div className='flex flex-col'><p className='m-2 text-xl font-bold'>Wednesday</p><p>{wedcount}</p><button onClick={() => setWedcount(wedcount + 1)}>Add to Wednesday</button></div>
+        <div className='flex flex-col'><p className='m-2 text-xl font-bold'>Thursday</p><p>{thucount}</p><button onClick={() => setThucount(thucount + 1)}>Add to Thursday</button></div>
+        <div className='flex flex-col'><p className='m-2 text-xl font-bold'>Friday</p><p>{fricount}</p><button onClick={() => setFricount(fricount + 1)}>Add to Friday</button></div>
+        <div className='flex flex-col'><p className='m-2 text-xl font-bold'>Saturday</p><p>{satcount}</p><button onClick={() => setSatcount(satcount + 1)}>Add to Saturday</button></div>
+        <div className='flex flex-col'><p className='m-2 text-xl font-bold'>Sunday</p><p>{suncount}</p><button onClick={() => setSuncount(suncount + 1)}>Add to Sunday</button></div>
+        </div>
+      }
+
         </div>
       </div>
 
